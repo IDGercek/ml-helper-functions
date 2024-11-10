83663c46bd69cf8f9760ddb14d6f7f6171e0dc62
IDGercek <gercekilhan44@gmail.com>
2024-11-10 19:24:03 +0300

    data manipulation update

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
-def normalize_dataframe(df: pd.DataFrame):
+def normalize(x):
     """
         Normalizes a Pandas DataFrame using mean normalization.
+        This object should have .mean() and .std() functions available.
     """
-    return (df-df.mean())/df.std()
+    return (x-x.mean())/x.std()
 
-def normalize_dataframe_column(col: pd.Series):
+def revert_normalization(x):
     """
-        Normalizes a Pandas DataFrame column using mean normalization.
+        Reverts the normalization operation applied to an object.
+        This object should have .mean() and .std() functions available.
     """
-    return (col-col.mean())/col.std()
+    return (x * x.std()) / x.mean()
